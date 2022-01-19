6a21300b7355ff1a5441800f5390b4a6191b9c4a
huycans <huycan_lk@yahoo.com.vn>
2022-01-19 11:58:34 -0500

    Add equityProfile

--- src/components/Profile/EquityProfile.tsx
+++ src/components/Profile/EquityProfile.tsx
@@ -1,5 +1,51 @@
-import React from 'react';
+import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import {
+  selectTickerInfo,
+  TickerInfoType
+} from "../../redux/slices/tickerSlice";
+import { formatDate, formatMoney, formatNumber, formatPercent } from "../utils";
 
 export default function EquityProfile() {
-  return <div>EquityProfile</div>;
+  const tickerInfo = useSelector(selectTickerInfo) as TickerInfoType;
+  const {
+    longName,
+    address1,
+    city,
+    state,
+    zip,
+    phone,
+    website,
+    sector,
+    industry,
+    fullTimeEmployees,
+    country
+  } = tickerInfo;
+
+  return (
+    <div className="equityProfile profile">
+      <h3>
+        <b>{longName}</b>
+      </h3>
+      <div className="row">
+        <div className="col-md-6 col-12">
+          {address1} <br />
+          {city} {state} {zip}
+          <br />
+          {country}
+          <br />
+          {phone}
+          <br />
+          {website}
+          <br />
+        </div>
+
+        <div className="col-md-6 col-12">
+          Sector: <span className="fw-bold">{sector}</span> <br />
+          Industry: <span className="fw-bold">{industry}</span> <br />
+          Fulltime employees: <span className="fw-bold">{formatNumber(fullTimeEmployees)}</span> <br />
+        </div>
+      </div>
+    </div>
+  );
 }
