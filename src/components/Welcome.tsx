7e154a242a1788f47b1bdec8c0e38c341be92d04
huycans <huycan_lk@yahoo.com.vn>
2022-01-29 10:59:30 -0500

    Redirect to summay page when search for 1st time

--- src/components/Welcome.tsx
+++ src/components/Welcome.tsx
@@ -1,13 +1,20 @@
 import React from "react";
+import { selectTickerInfo } from "../redux/slices/tickerSlice";
+import { useSelector } from "react-redux";
+import { Navigate } from "react-router-dom";
 
 export default function Welcome() {
-  return (
-    <div>
-      <h1>Welcome to Stockviewer.</h1>
-      <p>
-        Enter a ticker for a single stock, mutual fund or ETF on the search bar
-        to see information about that ticker
-      </p>
-    </div>
-  );
+  const tickerInfo = useSelector(selectTickerInfo);
+  if (tickerInfo === null) {
+    return (
+      <div>
+        <h1>Welcome to Stockviewer.</h1>
+        <p>
+          Enter a ticker for a single stock, mutual fund or ETF on the search
+          bar to see information about that ticker
+        </p>
+      </div>
+    );
+  }
+  return <Navigate to={"/summary"} />;
 }
