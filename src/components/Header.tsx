15c7063453709961fff30f62f277a92c6792db66
huycans <huycan_lk@yahoo.com.vn>
2022-02-16 15:55:38 -0500

    New header links

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,104 +2,33 @@ import React, { useState, useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSearch } from "@fortawesome/free-solid-svg-icons";
-import { NavLink, useNavigate } from "react-router-dom";
+import { Link, NavLink, useNavigate } from "react-router-dom";
 import classnames from "classnames";
 
 import { getTickerInfo, selectTickerInfo } from "../redux/slices/tickerSlice";
 import stockviewerLogo from "../assets/img/stockviewer.png";
 
 export default function Header() {
-  const [tickerName, setTickerName] = useState("VTHRX");
-  const tickerInfo = useSelector(selectTickerInfo);
-
-  const dispatch = useDispatch();
-  let navigate = useNavigate();
-
-  const handleTickerSearch = () => {
-    dispatch(getTickerInfo(tickerName));
-    navigate("/summary");
-  };
-
-  const handleChange = (event: React.ChangeEvent) => {
-    let target = event.target as HTMLInputElement;
-    setTickerName(target.value);
-  };
-
-  const handleKeyPressed = (event: React.KeyboardEvent) => {
-    // Number 13 is the "Enter" key on the keyboard
-    if (event.key === "Enter" || event.keyCode === 13) {
-      // Cancel the default action, if needed
-      event.preventDefault();
-      handleTickerSearch();
-    }
-  };
-
-  const navItems = [
-    {
-      to: "/summary",
-      name: "Summary"
-    },
-    {
-      to: "/chart",
-      name: "Chart"
-    },
-    {
-      to: "/profile",
-      name: "Profile"
-    }
-  ];
-
-  if (tickerInfo?.quoteType === "ETF"|| tickerInfo?.quoteType === "MUTUALFUND") navItems.push({
-    to: "/holdings",
-    name: "Holdings"
-  })
-
   return (
     <header className="row">
-      <div className="row">
-        <div className="col-3 header-title">
+      <div className="col-6 header-title">
+        <Link to={"/"}>
           <img
             id="stockviewerLogo"
             src={stockviewerLogo}
             alt="Stockviewer logo"
           />
-        </div>
-        <div className="col-6 header-searchBar">
-          <input
-            value={tickerName}
-            id="searchBar"
-            type="text"
-            onChange={handleChange}
-            placeholder="Enter a stock ticker here"
-            onKeyUp={handleKeyPressed}
-          />
-          <button id="searchBtn" onClick={handleTickerSearch}>
-            <FontAwesomeIcon icon={faSearch} />
-          </button>
-        </div>
-        <div className="col-3 header-blank"></div>
+        </Link>
       </div>
-      <div className="row">
-        {tickerInfo ? (
-          <ul className="nav nav-tabs justify-content-center">
-            {navItems.map((navItem) => {
-              return (
-                <li className="nav-item" key={navItem.name}>
-                  <NavLink
-                    className={({ isActive }) =>
-                      classnames("nav-link", {
-                        active: isActive
-                      })
-                    }
-                    to={navItem.to}
-                  >
-                    {navItem.name}
-                  </NavLink>
-                </li>
-              );
-            })}
-          </ul>
-        ) : null}
+      <div className="col-3">
+        <Link to={"/research"}>
+          <h5>Research a security</h5>
+        </Link>
+      </div>
+      <div className="col-3">
+        <Link to={"/compare"}>
+          <h5>Compare funds</h5>
+        </Link>
       </div>
     </header>
   );
