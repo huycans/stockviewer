e66ada734f536e1faae80d0d74ff334d813f37fe
huycans <huycan_lk@yahoo.com.vn>
2022-02-20 12:09:05 -0500

    Put new search bar in Research

--- src/components/Research.tsx
+++ src/components/Research.tsx
@@ -6,6 +6,7 @@ import { NavLink, useNavigate, Outlet } from "react-router-dom";
 import classnames from "classnames";
 
 import { getTickerInfo, selectTickerInfo } from "../redux/slices/tickerSlice";
+import SearchBar from "./SearchBar";
 
 export default function Research() {
   const [tickerName, setTickerName] = useState("VTHRX");
@@ -21,15 +22,6 @@ export default function Research() {
   const handleChange = (event: React.ChangeEvent) => {
     let target = event.target as HTMLInputElement;
     setTickerName(target.value);
-  };
-
-  const handleKeyPressed = (event: React.KeyboardEvent) => {
-    // Number 13 is the "Enter" key on the keyboard
-    if (event.key === "Enter" || event.keyCode === 13) {
-      // Cancel the default action, if needed
-      event.preventDefault();
-      handleTickerSearch();
-    }
   };
 
   const navItems = [
@@ -56,26 +48,17 @@ export default function Research() {
   return (
     <div className="container">
       <div className="row">
-        <div className="row searchBar">
-          <div className="col-2 blank"></div>
-          <div className="col-8">
-            <label className="search-label" htmlFor="searchBar">
-              Ticker symbol: 
-            </label>
-            <input
-                value={tickerName}
-                id="searchBar"
-                type="text"
-                onChange={handleChange}
-                placeholder="Enter a stock ticker here"
-                onKeyUp={handleKeyPressed}
-              />
-            <button id="searchBtn" onClick={handleTickerSearch}>
-              <FontAwesomeIcon icon={faSearch} />
-            </button>
-          </div>
-          <div className="col-2 blank"></div>
+        <div className="row">
+          <SearchBar
+            value={tickerName}
+            onChange={handleChange}
+            handleSearch={handleTickerSearch}
+            label={"Ticker symbol:"}
+            searchBarId="searchBar"
+            placeholder="Enter a stock ticker here"
+          />
         </div>
+
         <div className="row research-tabs">
           {tickerInfo ? (
             <ul className="nav nav-tabs justify-content-center">
