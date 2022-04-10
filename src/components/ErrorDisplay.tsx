fbd9a23efec31f5409afb308f43c3668ff40e4d3
huycans <huycan_lk@yahoo.com.vn>
2022-04-10 10:58:24 -0400

    ErrorDisplay reminder

--- src/components/ErrorDisplay.tsx
+++ src/components/ErrorDisplay.tsx
@@ -1,8 +1,12 @@
 import React from "react";
 import { useSelector } from "react-redux";
-import {selectTickerError} from '../redux/slices/tickerSlice'
+import { useLocation, Link } from "react-router-dom";
+import {selectTickerError} from '../redux/slices/tickerSlice';
+import {} from '../redux/slices/compareSlice';
 
 export default function ErrorDisplay() {
+  const location = useLocation();
+  //TODO: read location, then display error based on location
   const tickerError = useSelector(selectTickerError);
   if (tickerError !== "") return <h3 className="errorContainer">{tickerError}</h3>
   else return null;
